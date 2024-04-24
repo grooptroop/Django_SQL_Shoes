f93f526e0d5b38add9d51522208b26c4e81a586f
vvallker <96576891+grooptroop@users.noreply.github.com>
2024-04-25 00:35:16 +0300

    start project 0.0.4 added mysql bd

--- Music_SQL/main/models.py
+++ Music_SQL/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class Artists(models.Model):
+    name = models.CharField(max_length=128)
+    date_of_start = models.DateField()
+    date_of_end = models.DateField()
 # Create your models here.
